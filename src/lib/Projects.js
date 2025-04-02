1a354fed32756253433229f72a85a40a9993f2bf
tentorfield <hngo017@ucr.edu>
2025-04-02 12:38:57 -0700

    Add js files

---
+++ src/lib/Projects.js
@@ -0,0 +1,26 @@
+// import PdfViewer from 'svelte-pdf';
+
+const projects = [
+	{
+		title: 'CarMD App and Responsive Website Case Study',
+		technologies: ['Javascript', 'HTML', 'CSS', 'AdobeXD'],
+		description:
+			'Evaluating the usage of responsive websites so that the look and feel is consistent for all devices',
+		url: 'https://docs.google.com/presentation/d/1a333yg0WDfy3aJBE8H7OdEEtxn3dg0KJ/edit?usp=share_link&ouid=101689789526322701913&rtpof=true&sd=true'
+	},
+	{
+		title: 'RepairSolutionsPRO™',
+		technologies: ['Javascript', 'HTML', 'CSS', 'Figma'],
+		description:
+			'RepairSolutionsPRO™ delivers pro-level features and functions that maximize the diagnostic process.',
+		url: 'https://pro.repairsolutions.com/'
+	},
+	{
+		title: 'Innova 7111',
+		technologies: ['Javascript', 'HTML', 'CSS', 'Axure'],
+		description: 'The Innova 7111 Smart Diagnostic System provides enthusiast DIYer’s and pros with OEM level services, in an advanced and easy-to-use tablet form factor with an intuitive user interface',
+		url: 'https://www.innova.com/products/smart-diagnostic-system-obd2-tablet-7111'
+	}
+];
+
+export default projects;
---
+++ src/lib/Skills.js
@@ -0,0 +1,10 @@
+const skills = {
+	programming: ['C++', 'Python', 'Node.js', 'Javascript', 'Sveltekit', 'Ruby', 'C#'],
+	// devops: [],
+	databases: ['SQLITE', 'Microsoft SQL Server'],
+	// tools: [],
+	languages: ['Vietnamese', 'Mandarin', 'English'],
+	other: ['Agile', 'Design principles', 'User interface design', 'Product Management']
+};
+
+export default skills;
